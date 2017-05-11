444a15250cdd52883001334cd2d3042c0a2c71a5
paprickar <patrick.kolze@akelius.de>
2017-05-11 18:10:02 +0200

    add details endpoint and jolokia

--- src/main/java/com/dmt/IfconfigController.java
+++ src/main/java/com/dmt/IfconfigController.java
@@ -2,6 +2,9 @@ package com.dmt;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Enumeration;
+
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
@@ -11,10 +14,29 @@ public class IfconfigController {
     public String Ifconfig(HttpServletRequest request) {
     	//is client behind something?
     	String ipAddress = request.getHeader("X-FORWARDED-FOR");
+    	
     	if (ipAddress == null) {
     		   ipAddress = request.getRemoteAddr();
     	}
     	
     	return ipAddress;
     }
+    
+    @RequestMapping("/details")
+    public String IfconfigDetail(HttpServletRequest request) {
+    	String response = "";
+    	
+    	//String userAgent = request.getHeader("User-Agent");
+    	
+    	Enumeration<String> headerNames = request.getHeaderNames();
+    	if (headerNames != null) {
+            while (headerNames.hasMoreElements()) {
+            	 	String header = headerNames.nextElement();
+                    response = response + " " + header +  ": " + request.getHeader(header);
+            }
+    	}
+    	
+    	return response;
+    }
+    
 }
